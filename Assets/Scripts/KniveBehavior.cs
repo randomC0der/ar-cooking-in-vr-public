f720c1e4b6057fcfc8c0c116f8793328ba99f7e3
Lukas Hein <lukashein@protonmail.com>
2023-01-19 00:11:40 +0100

    Zwischenstand

---
+++ Assets/Scripts/KniveBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Assets.Scripts
+{
+    class KniveBehavior : MonoBehaviour
+    {
+        bool _insideCuttingBoard;
+
+        public void EnterCuttingBoardCollision(HoverEnterEventArgs e)
+        {
+            XRBaseInteractor interactor = e.interactor;
+            if (interactor.hasSelection && !_insideCuttingBoard)
+            {
+                CuttingBehavior cuttingBehavior = interactor.firstInteractableSelected.transform.gameObject.GetComponent<CuttingBehavior>();
+                if (!(cuttingBehavior is null)) {
+                    cuttingBehavior.Cut();
+                }
+            }
+            _insideCuttingBoard = true;
+        }
+
+        public void ExitCuttingBoardCollision()
+        {
+            _insideCuttingBoard = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+static class Util
+{
+    public static GrabableGameObject AddGrabableComponents(this GameObject go)
+    {
+        go.AddComponent<MeshCollider>().convex = true;
+        Rigidbody rigid = go.AddComponent<Rigidbody>();
+        XRGrabInteractable grab = go.AddComponent<XRGrabInteractable>();
+        grab.attachTransform = go.transform.Find("Attatch Transform");
+
+        return new GrabableGameObject
+        {
+            GameObject = go, 
+            XrGrab = grab,
+            Rigidbody = rigid
+        };
+    }
+}
+
+public struct GrabableGameObject
+{
+    public GameObject GameObject;
+    public XRGrabInteractable XrGrab;
+    public Rigidbody Rigidbody;
+}
